7cb84041cdf34ef9768420aa2e7c01fb20e06522
Anton Abbasov <antonabbasov@gmail.com>
2021-12-07 17:33:33 +0300

    Revert "TODO: Async requests"
    This reverts commit dec336b243b3b861f0664e09ba4e1c51f7248194.

--- Movie Base/Authentication Module/Auth SignUp/View/SignUpViewController.swift
+++ Movie Base/Authentication Module/Auth SignUp/View/SignUpViewController.swift
@@ -9,9 +9,7 @@ import UIKit
 import FirebaseAuth
 import Firebase
 
-class SignUpViewController: UIViewController, LoginViewControllerInput {
-    
-    var signUpViewOutput: SignUpViewControllerOutput!
+class SignUpViewController: UIViewController {
 
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -45,16 +43,64 @@ class SignUpViewController: UIViewController, LoginViewControllerInput {
         // Pass the selected object to the new view controller.
     }
     */
+    func validateTheFiedls() -> String? {
+        
+        if (firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)) == "" ||
+            (lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)) == "" ||
+            (emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)) == "" ||
+            (passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)) == "" {
+            
+            return "Please filled in all fields."
+        }
+        
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if !Utilities.isPasswordValid(cleanedPassword) {
+            return "Please make sure your password is at least 8 characters< contains a speial character and a number."
+        }
+        
+        return nil
+    }
     
     func showError(_ message: String) {
         errorLabel.text = message
         errorLabel.isHidden = false
     }
     
+    func transitionToHome() {
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homveViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+    }
     
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        let error = validateTheFiedls()
         
-        signUpViewOutput.didPressSignUp(email: emailTextField.text, password: passwordTextField.text, firstname: firstNameTextField.text, lastname: lastNameTextField.text)
+        if error != nil {
+            showError(error!)
+        } else {
+            
+            let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            Auth.auth().createUser(withEmail: email, password: password) { result, err in
+                if err != nil {
+                    self.showError("Error creating user.")
+                } else {
+                    let db = Firestore.firestore()
+                    db.collection("users").addDocument(data: ["firstname": firstName, "lastname": lastName, "UID": result!.user.uid]) { error in
+                        if error != nil {
+                            self.showError("Error saving user data") //юзер был создан но пользователь не сможет зайти надо подумать
+                        }
+                        self.transitionToHome()
+                    }
+                }
+            }
+        }
+        
     }
-        
+    
 }
--- Movie Base/Data Module/FirebaseManager.swift
+++ Movie Base/Data Module/FirebaseManager.swift
@@ -11,37 +11,12 @@ import Firebase
 
 class FirebaseManager {
     func loginUser(email: String, password: String) -> String? {
-        
         var loginError: String?
-        
-        
-        Auth.auth().signIn(withEmail: email, password: password) {
-            (result, error) in
-            
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if error != nil {
-                loginError = error?.localizedDescription
-            } else {
-                let result = result
+                loginError = "Error logining user."
             }
         }
         return loginError
     }
-    
-    func signUpUser(email: String, password: String, firstname: String, lastname: String) -> String? {
-        var signUpError: String?
-        Auth.auth().createUser(withEmail: email, password: password) { result, err in
-            if err != nil {
-                signUpError = "Error creating user."
-            } else {
-                let db = Firestore.firestore()
-                db.collection("users").addDocument(data: ["firstname": firstname, "lastname": lastname, "UID": result!.user.uid]) { error in
-                    if error != nil {
-                        signUpError = "Error saving user data" //юзер был создан но пользователь не сможет зайти надо подумать
-                    }
-                }
-            }
-    }
-        return signUpError
 }
-}
-
